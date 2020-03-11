636d0348f7b0c6663150b76886493f16f135bb4f
Ricart <ricarteusuo11@gmail.com>
2020-03-11 11:08:50 -0300

    registering user on admin site

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from django.contrib.auth import admin as auth
+from .models import UUIDUser
 # Register your models here.
+
+
+# UserAdmin
+# - - - - - - - - - - - - - - - - - - -
+class UserAdmin(auth.UserAdmin):
+
+    fieldsets = (
+        ('Personal info', {'fields': ('username', 'password', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    filter_horizontal = ('groups', 'user_permissions')
+
+
+# Register
+# - - - - - - - - - - - - - - - - - - -
+admin.site.register(UUIDUser, UserAdmin)
