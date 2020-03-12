a22b85e7d14ef5c4fd248c21eac7a79e6273ac44
Ricart <ricarteusuo11@gmail.com>
2020-03-11 22:53:52 -0300

    adding item CRUD

--- app/game/urls.py
+++ app/game/urls.py
@@ -7,10 +7,17 @@ app_name = 'game'
 urlpatterns = [
     # Concept
     path('concepts/', views.ConceptsView.as_view(), name='concept-list'),
-    path('concepts/deleted', views.ConceptsDeletedView.as_view(), name='concept-list'),
+    path('concepts/deleted', views.ConceptsDeletedView.as_view(), name='concept-deleted'),
     path('concept/new', views.ConceptCreateView.as_view(), name='concept-register'),
     path('concept/<uuid:pk>/edit', views.ConceptEditView.as_view(), name='concept-edit'),
     path('concept/<uuid:pk>/delete', views.concept_delete_view, name='concept-delete'),
     path('concept/<uuid:pk>/activate', views.concept_activate_view, name='concept-activate'),
 
+    # Item
+    path('items/', views.ItemsView.as_view(), name='item-list'),
+    path('items/deleted', views.ItemsDeletedView.as_view(), name='item-deleted'),
+    path('item/new', views.ItemCreateView.as_view(), name='item-register'),
+    path('item/<uuid:pk>/edit', views.ItemEditView.as_view(), name='item-edit'),
+    path('item/<uuid:pk>/delete', views.item_delete_view, name='item-delete'),
+    path('item/<uuid:pk>/activate', views.item_activate_view, name='item-activate'),
 ]
--- app/game/views.py
+++ app/game/views.py
@@ -87,3 +87,116 @@ def concept_activate_view(request, pk):
     concept.active = True
     concept.save()
     return redirect('game:concept-list')
+
+
+# Items list
+# - - - - - - - - - - - - - - - - - - - -
+class ItemsView(ListView):
+    model = Item
+    template_name = 'game/item/list.html'
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect('core:dashboard')
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        kwargs['object_list'] = Item.objects.filter(active=True)
+        kwargs['type'] = "Active"
+        return super(ItemsView, self).get_context_data(**kwargs)
+
+
+# Items deleted list
+# - - - - - - - - - - - - - - - - - - - -
+class ItemsDeletedView(ListView):
+    model = Item
+    template_name = 'game/item/list.html'
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect('core:dashboard')
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        kwargs['object_list'] = Item.objects.filter(active=False)
+        kwargs['type'] = "Inactive"
+        return super(ItemsDeletedView, self).get_context_data(**kwargs)
+
+
+# Item register
+# - - - - - - - - - - - - - - - - - - - -
+class ItemCreateView(CreateView):
+    model = Item
+    template_name = 'game/item/form.html'
+    fields = ['name']
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect('core:dashboard')
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        kwargs['concepts'] = Concept.objects.all()
+        return super(ItemCreateView, self).get_context_data(**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        # Creating a instance of item
+        item = Item()
+        # Setting the name by the form
+        item.name = request.POST.get('name')
+        item.save()
+        # Going through concepts and checking if they are marked
+        concepts = []
+        for concept in Concept.objects.all():
+            con = request.POST.get(concept.name)
+            if con is not None:
+                c = Concept.objects.get(id=con)
+                c.save()
+                concepts.append(c)
+        # Setting concepts in Item instance
+        item.concepts.set(concepts)
+        item.save()
+        return redirect('game:item-list')
+
+
+# Item edit
+# - - - - - - - - - - - - - - - - - - - -
+class ItemEditView(UpdateView):
+    model = Item
+    template_name = 'game/item/update.html'
+    success_url = reverse_lazy('game:item-list')
+    fields = ['name', 'concepts']
+
+    # TODO fix item update
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect('core:dashboard')
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        kwargs['concepts'] = Concept.objects.all()
+        kwargs['marked'] = Item.objects.get(id=self.object.id).concepts.filter()
+        return super(ItemEditView, self).get_context_data(**kwargs)
+
+
+# Concept delete
+# - - - - - - - - - - - - - - - - - - - -
+def item_delete_view(request, pk):
+    if not request.user.is_staff:
+        return redirect('core:dashboard')
+    item = Item.objects.get(id=pk)
+    item.active = False
+    item.save()
+    return redirect('game:item-list')
+
+
+# Concept activate
+# - - - - - - - - - - - - - - - - - - - -
+def item_activate_view(request, pk):
+    if not request.user.is_staff:
+        return redirect('core:dashboard')
+    item = Item.objects.get(id=pk)
+    item.active = True
+    item.save()
+    return redirect('game:item-list')
